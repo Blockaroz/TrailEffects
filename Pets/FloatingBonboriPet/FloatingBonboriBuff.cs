af834d352bc22db56f343f3c8cc994ed47f8dbad
Blockaroz <lbpk.123fan@gmail.com>
2021-04-07 09:30:19 -0400

    fix

--- Pets/FloatingBonboriPet/FloatingBonboriBuff.cs
+++ Pets/FloatingBonboriPet/FloatingBonboriBuff.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
 using TrailEffects.ModPlayers;
 
@@ -21,15 +22,9 @@ namespace TrailEffects.Pets.FloatingBonboriPet
             player.buffTime[buffIndex] = 18000;
 
             int projType = ModContent.ProjectileType<FloatingBonboriProj>();
+            Vector2 pos = new Vector2(player.position.X + player.width / 2f, player.position.Y + player.height / 2f);
             if (player.ownedProjectileCounts[projType] <= 0 && player.whoAmI == Main.myPlayer)
-                Projectile.NewProjectile(player.position.X + player.width / 2f, player.position.Y + player.height / 2f,
-                    0f, 0f, ModContent.ProjectileType<FloatingBonboriProj>(), 0, 0f, player.whoAmI);
-
-            /*if (!player.controlDown || !player.releaseDown || player.doubleTapCardinalTimer[0] <= 0 ||
-                player.doubleTapCardinalTimer[0] == 15) return;
-
-            foreach (Projectile projectile in Main.projectile.Where(proj => proj.active && proj.type == ModContent.ProjectileType<FloatingBonboriProj>() && proj.owner == player.whoAmI))
-                projectile.velocity += 5f * Vector2.Normalize(Main.MouseWorld - projectile.Center);*/
+                Projectile.NewProjectileDirect(player.GetProjectileSource_Buff(buffIndex), pos, Vector2.Zero, ModContent.ProjectileType<FloatingBonboriProj>(), 0, 0f, player.whoAmI);
         }
     }
 }
